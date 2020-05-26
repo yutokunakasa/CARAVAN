cf4dcab5837119ee770be957f1c4dd0576af61cf
仲佐　祐德 <yutokunakasa@nakatasukuyuunoMacBook-Air.local>
2020-05-27 01:44:34 +0900

    [update]4-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,7 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def index
@@ -16,7 +17,7 @@ class BlogsController < ApplicationController
   def create
   	blog = Blog.new(blog_params)
   	blog.save
-  	redirect_to blogs_path
+  	redirect_to blogs_path(blog.id)
   end
 
   def edit
