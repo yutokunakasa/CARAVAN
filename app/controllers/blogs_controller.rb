779f0fb231c0da04de6c0f79541f75e90eb63120
仲佐　祐德 <yutokunakasa@nakatasukuyuunoMacBook-Air.local>
2020-05-27 02:26:16 +0900

    [update]4-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -29,6 +29,11 @@ class BlogsController < ApplicationController
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
 
   private
   def blog_params
